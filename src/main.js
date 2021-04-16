3396b78355c8e18359e77555b2bcb21d225325e3
liudaming <liudaming@dott-tech.com>
2021-04-16 22:43:12 +0800

    feat: 初始化项目结构

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
+import Antd from "ant-design-vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import "./assets/less/index.less";
+
+createApp(App).use(Antd).mount("#app");
